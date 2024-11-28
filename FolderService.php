831137c3c21ac42ba24708e060d26a24ed8d46be
Marco <49117072+guillardmarc@users.noreply.github.com>
2024-11-28 16:04:45 +0100

    Floder Services v1
    Test:
    - Symfony 7.1
    - PHP 8.3.6

---
+++ FolderService.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
+
+class FolderService
+{    
+    public function __construct(
+        private Filesystem $filesystem,
+    )
+    {}
+
+    public function createdFolder($path, $folderName)
+    {
+        if (!$this->filesystem->exists($path)) { 
+            $this->filesystem->mkdir($path, 0700);
+            return '"'.$folderName.'" folder created';
+        } 
+        elseif ($this->filesystem->exists($path)) {
+            return 'Folder already exists';
+        }
+        else {
+            return 'Error while trying to verify existence and create desired folder' ;
+        }
+    }
+
+    public function renameFolder ($oldPath, $OldFolderName, $path, $folderName)
+    {
+        if ($this->filesystem->exists($oldPath) && !$this->filesystem->exists($path)) {
+            $this->filesystem->rename($oldPath,$path);
+            return 'Folder renamed';
+        }
+    }
+
+    public function deletedFolder($path, $folderName)
+    {
+        if (!file_exists($path)) { 
+            return false; 
+        }
+        if (!is_dir($path)) { 
+            return unlink($path); 
+        } 
+        foreach (scandir($path) as $item) { 
+            if ($item == '.' || $item == '..') { 
+                continue; 
+            }
+            else {
+                unlink( $path.'/'.$item );
+            }
+        }
+        rmdir($path);
+        return 'The "'.$folderName.'" folder has been deleted';
+    }
+}
